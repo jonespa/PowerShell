33010f40f3eab01dc242f50083266783b70b6e4f
duddit2 <58218990+duddit2@users.noreply.github.com>
2020-01-08 14:52:20 +0000

    Update Connect-local-exchnage.ps1

--- EXCHANGE/Connect-local-exchnage.ps1
+++ EXCHANGE/Connect-local-exchnage.ps1
@@ -1,2 +1,3 @@
+Set-ExecutionPolicy RemoteSigned
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://hf-ex01/PowerShell/ -Authentication Kerberos
 Import-PSSession $Session
