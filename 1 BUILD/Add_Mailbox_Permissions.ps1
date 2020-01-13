fb074d3b9faa5ec9bb769913fe3a3300f33acef0
duddit2 <58218990+duddit2@users.noreply.github.com>
2020-01-13 13:48:11 +0000

    Update Add_Mailbox_Permissions.ps1

--- 1 BUILD/Add_Mailbox_Permissions.ps1
+++ 1 BUILD/Add_Mailbox_Permissions.ps1
@@ -1,17 +1,19 @@
-#Set-ExecutionPolicy RemoteSigned
-#$UserCredential = Get-Credential
-#$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
-#Import-PSSession $Session -DisableNameChecking
+Set-ExecutionPolicy RemoteSigned
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session -DisableNameChecking
 
-#$shared = read-host "Enter Shared Mailbox Name"
-#$User = read-host "Enter Full Name of User";
+$shared = read-host "Enter Shared Mailbox Name"
+$User = read-host "Enter Full Name of User";
+
+# https://www.business.com/articles/powershell-interactive-menu/
 
 function Show-Menu
 {
      param (
-           [string]$Title = 'Permission level'
+           [string]$Title = 'My Menu'
      )
-     cls
+     Clear-Host
      Write-Host "================ $Title ================"
     
      Write-Host "1: Press '1' for Full Access."
@@ -19,6 +21,29 @@ function Show-Menu
      Write-Host "3: Press '3' for Send on Behalf of."
      Write-Host "Q: Press 'Q' to quit."
 }
+
+do
+{
+     Show-Menu
+     $input = Read-Host "Please make a selection"
+     switch ($input)
+     {
+           '1' {
+                Clear-Host
+                'You chose option #1'
+           } '2' {
+                Clear-Host
+                'You chose option #2'
+           } '3' {
+                Clear-Host
+                'You chose option #3'
+           } 'q' {
+                return
+           }
+     }
+     pause
+}
+until ($input -eq 'q')
 
 
 
