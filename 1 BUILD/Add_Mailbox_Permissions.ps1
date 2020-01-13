714082e00af15daca526b646dd60745579ef1915
duddit2 <58218990+duddit2@users.noreply.github.com>
2020-01-13 12:29:06 +0000

    Update Add_Mailbox_Permissions.ps1

--- 1 BUILD/Add_Mailbox_Permissions.ps1
+++ 1 BUILD/Add_Mailbox_Permissions.ps1
@@ -1,9 +1,25 @@
-Set-ExecutionPolicy RemoteSigned
-$UserCredential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
-Import-PSSession $Session -DisableNameChecking
+#Set-ExecutionPolicy RemoteSigned
+#$UserCredential = Get-Credential
+#$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+#Import-PSSession $Session -DisableNameChecking
 
-$shared = read-host "Enter Shared Mailbox Name"
-$User = read-host "Enter Full Name of User";
+#$shared = read-host "Enter Shared Mailbox Name"
+#$User = read-host "Enter Full Name of User";
 
-Add-MailboxPermission -identity "$Shared" -User "$User" -AccessRights FullAccess -InheritanceType all
+function Show-Menu
+{
+     param (
+           [string]$Title = 'Permission level'
+     )
+     cls
+     Write-Host "================ $Title ================"
+    
+     Write-Host "1: Press '1' for Full Access."
+     Write-Host "2: Press '2' for Send As."
+     Write-Host "3: Press '3' for Send on Behalf of."
+     Write-Host "Q: Press 'Q' to quit."
+}
+
+
+
+#Add-MailboxPermission -identity "$Shared" -User "$User" -AccessRights FullAccess -InheritanceType all
