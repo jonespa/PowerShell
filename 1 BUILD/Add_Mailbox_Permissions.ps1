0bad839de1c9655c3d089042fc91d6c8caaac3e8
duddit2 <58218990+duddit2@users.noreply.github.com>
2020-01-14 07:59:15 +0000

    Update Add_Mailbox_Permissions.ps1

--- 1 BUILD/Add_Mailbox_Permissions.ps1
+++ 1 BUILD/Add_Mailbox_Permissions.ps1
@@ -1,10 +1,10 @@
-Set-ExecutionPolicy RemoteSigned
-$UserCredential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
-Import-PSSession $Session -DisableNameChecking
+#Set-ExecutionPolicy RemoteSigned
+#$UserCredential = Get-Credential
+#$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+#Import-PSSession $Session -DisableNameChecking
 
-$shared = read-host "Enter Shared Mailbox Name"
-$User = read-host "Enter Full Name of User";
+#$shared = read-host "Enter Shared Mailbox Name"
+#$User = read-host "Enter Full Name of User";
 
 # https://www.business.com/articles/powershell-interactive-menu/
 
@@ -29,21 +29,23 @@ do
      switch ($input)
      {
            '1' {
-                Clear-Host
-                "Do you want to grant $User Full access permission on $Shared Mailbox?" | Add-MailboxPermission -identity "$Shared" -User "$User" -AccessRights FullAccess -InheritanceType all
+                Clear-Host 
+                $confirm = Read-Host "Do you want to grant $User Full access permission on $Shared Mailbox? (Y/N)" 
+                if ($confirm -eq "Y"){Add-MailboxPermission -identity "$Shared" -User "$User" -AccessRights FullAccess -InheritanceType all}
+                elseif ($confirm -eq "N"){return}
+                return
+                 
 
            }
 
-         
-           
            '2' {
                 Clear-Host
-                "Do you want to grant $User Send-As permission on $Shared Mailbox?" | Add-MailboxPermission -identity "$Shared" -User "$User" -AccessRights Send-As -InheritanceType all
+                Read-Host "Do you want to grant $User Send-As permission on $Shared Mailbox?" | Add-MailboxPermission -identity "$Shared" -User "$User" -AccessRights Send-As -InheritanceType all
            } 
            
            '3' {
                 Clear-Host
-                "Do you want to grant $User Send on Behalf of permission on $Shared Mailbox?" | Add-MailboxPermission -identity "$Shared" -User "$User" -AccessRights Send-As -InheritanceType all
+                Read-Host "Do you want to grant $User Send on Behalf of permission on $Shared Mailbox?" | Add-MailboxPermission -identity "$Shared" -User "$User" -AccessRights Send-As -InheritanceType all
            } 'q' {
                 return
            }
